0994cabfe6f5e583fd7f42e8497d7b7a3ed9636b
Antu Saha <antusaha970@gmail.com>
2025-01-15 15:47:59 +0600

    added home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <h1 className="text-center">
-      Send bulk message from our website using your configuration
-    </h1>
+    <div className="container my-5">
+      <h1 className="text-center">
+        Send bulk message from our website using your configuration
+      </h1>
+      <div className="d-flex justify-content-center align-items-center">
+        <Link to={"/add_mails"} className="btn btn-primary btn-lg">
+          Get started
+        </Link>
+      </div>
+    </div>
   );
 };
 
