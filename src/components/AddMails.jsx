548d3304ad377bc801283aba3dc523e676f32830
Antu Saha <antusaha970@gmail.com>
2025-01-16 12:00:39 +0600

    added excel file

--- src/components/AddMails.jsx
+++ src/components/AddMails.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { EmailAddressContext } from "../context";
 import { useForm } from "react-hook-form";
+import client from "../client";
 
 const AddMails = () => {
   const { register, handleSubmit, reset } = useForm();
@@ -10,9 +11,32 @@ const AddMails = () => {
     setMail_address((prevState) => [...prevState, data["email"]]);
     reset();
   };
-  console.log(mail_address);
   const handleClearEmails = () => {
     setMail_address([]);
+  };
+
+  const handleExcelFileUpload = async (data) => {
+    try {
+      const formData = new FormData();
+      formData.append("file", data["file"][0]);
+      const response = await client.post(
+        "/email_address/email_address_list/",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      if (response.status == 200) {
+        const data = response?.data?.data;
+        setMail_address((prevState) => [...prevState, ...data]);
+        alert("mail added successfully");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("something went wrong");
+    }
   };
 
   return (
@@ -45,6 +69,29 @@ const AddMails = () => {
               id="button-addon2"
             >
               Clear
+            </button>
+          </div>
+        </form>
+        <form
+          className="mx-auto shadow p-4 rounded bg-white w-75 border-top border-4 border-success my-3"
+          onSubmit={handleSubmit(handleExcelFileUpload)}
+        >
+          <h4 className="text-center">Upload excel file</h4>
+          <div className="input-group mb-3">
+            <input
+              type="file"
+              className="form-control border-2"
+              placeholder="Recipient's email address"
+              aria-describedby="button-addon2"
+              required
+              {...register("file")}
+            />
+            <button
+              className="btn btn-success"
+              type="submit"
+              id="button-addon2"
+            >
+              Upload
             </button>
           </div>
         </form>
